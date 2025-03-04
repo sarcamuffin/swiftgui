5ee59106d828ad440906c17cf37172d1c4cae24f
sarcamuffin <douyhry.aya@student.emi.ac.ma>
2025-03-04 15:27:37 +0100

    first

---
+++ guiswift.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        architecture_value = request.form.get('architecture')
+        client_name_value = request.form.get('client_name')
+        value_2_4 = request.form.get('value_2_4')
+        
+        print(f"Architecture: {architecture_value}")
+        print(f"Client Name: {client_name_value}")
+        print(f"2.4: {value_2_4}")
+        
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
